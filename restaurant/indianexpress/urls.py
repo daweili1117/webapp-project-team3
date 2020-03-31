73347b9f59db05d83256a7d2ceca2d81b8b37614
jaswinder612 <59885074+jaswinder612@users.noreply.github.com>
2020-03-30 22:45:30 -0500

    Update urls.py

--- restaurant/indianexpress/urls.py
+++ restaurant/indianexpress/urls.py
@@ -7,6 +7,12 @@ app_name = 'indianexpress'
 urlpatterns = [
     path('', views.home, name='home'),
     re_path(r'^home/$', views.home, name='home'),
+    path('signup/', views.signup, name="signup"),
+    path('reservation/', views.reservation, name="reservation"),
+    path('menu/', views.menu, name="menu"),
+    path('cart/', views.cart, name="cart"),
+    path('gallery/', views.gallery, name="gallery"),
+
     ]
 
 
