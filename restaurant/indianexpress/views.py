423a0cd5b534eb6fa2b2cf4ed1920e492801b75c
jaswinder612 <59885074+jaswinder612@users.noreply.github.com>
2020-03-30 22:44:39 -0500

    Update views.py

--- restaurant/indianexpress/views.py
+++ restaurant/indianexpress/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 # Create your views here.
+
+from .forms import SignUpForm
+
+
+def home(request):
+   return render(request, 'indianexpress/home.html',
+                 {'indianexpress': home})
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('login')
+    else:
+        form = SignUpForm()
+    return render(request, 'indianexpress/signup.html', {'form': form})
+
+
+def reservation(request):
+    return render(request, 'indianexpress/reservation.html', {'indianexpress': reservation})
+
+
+def menu(request):
+    return render(request, 'indianexpress/menu.html', {'indianexpress': menu})
+
+
+def cart(request):
+    return render(request, 'indianexpress/cart.html', {'indianexpress': cart})
+
+def gallery(request):
+    return render(request, 'indianexpress/gallery.html', {'indianexpress': gallery})
