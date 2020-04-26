796e345cc74560fdab9cc1c37467b549d61bf214
jaswinder612 <59885074+jaswinder612@users.noreply.github.com>
2020-04-26 00:45:18 -0500

    Updated

--- restaurant/orders/forms.py
+++ restaurant/orders/forms.py
@@ -3,6 +3,13 @@ from .models import Order
 
 
 class OrderCreateForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=250)
+    last_name = forms.CharField(max_length=250)
+    email = forms.EmailField()
+    address = forms.CharField(max_length=255)
+    postal_code = forms.CharField(max_length=255)
+    city = forms.CharField(max_length=255)
+
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address',
